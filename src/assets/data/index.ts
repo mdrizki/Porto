0decac110fbc083f20c3a6ce056fd77f0ec75398
FaiqSr <FaiqSr@github.com>
2024-12-02 20:25:26 +0700

    add: data work experience, foto project

--- src/assets/data/index.ts
+++ src/assets/data/index.ts
@@ -1,3 +1,54 @@
+import porto1 from "../porto-foto/porto4.jpg"
+import porto2 from "../porto-foto/porto5.jpg"
+import porto3 from "../porto-foto/porto6.jpg"
+import porto4 from "../porto-foto/porto7.jpg"
+import porto5 from "../porto-foto/porto12.jpg"
+import porto6 from "../porto-foto/porto13.jpg"
+
+
+export const work = [
+    {
+        id: 1,
+        title: "Media Creative Indonesia",
+        date: "May 2024 - August 2024",
+        job: "Editor FTV",
+        jobdesk: "Program",
+        place: "Depok, Jawa Barat"
+    },
+    {
+        id: 2,
+        title: "Siaran Berita Televisi Redaksi 12",
+        date: "October 2023 - January 2024",
+        job: "Editor Redaksi 12",
+        jobdesk: "Program",
+        place: "DKI Jakarta"
+    },
+    {
+        id: 3,
+        title: "Renjana Production",
+        date: "October 2023 - November 2023",
+        job: "Editor, VideoGrapher",
+        jobdesk: "Freelance",
+        place: "DKI Jakarta"
+    },
+    {
+        id: 4,
+        title: "Mata Seni Production",
+        date: "March 2023 - June 2023",
+        job: "Editor FTV",
+        jobdesk: "Workshop",
+        place : "Depok, Jawa Barat"
+    },
+    {
+        id: 5,
+        title: "D'Mensen Industries",
+        date: "January 2021 - June 2023",
+        job: "Media Management",
+        jobdesk: "Probation",
+        place: "Bogor, Jawa Barat"
+    },
+]
+
 export const nav = [
     {
         id: 1,
@@ -14,9 +65,37 @@ export const nav = [
         name: "Projects",
         path: "/project",
     },
+]
+
+export const fotoGDrive = [
+    {
+        id: 1,
+        img: porto1,
+        title: "test"
+    },
+    {
+        id: 2,
+        img: porto2,
+        title: "test"
+    },
+    {
+        id: 3,
+        img: porto3,
+        title: "test"
+    },
     {
         id: 4,
-        name: "Contact",
-        path: "/contact",
+        img: porto4,
+        title: "test"
     },
+    {
+        id: 5,
+        img: porto5,
+        title: "test"
+    },
+    {
+        id: 6,
+        img: porto6,
+        title: "test"
+    }
 ]
